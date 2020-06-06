7e2ee891d6af30bf729c31ea22d9b7c7faf81cc7
Sergio Xalambrí <hello@sergiodxa.com>
2020-06-06 04:53:02 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { GetStaticProps, InferGetStaticPropsType } from "next";
 import Link from "next/link";
 import { Note } from "data/notes";
-import { readSite, Site } from "data/site";
+import { readSite, Site, useSite } from "data/site";
 
 export const getStaticProps: GetStaticProps<Site> = async () => {
   const { site, notes } = await readSite(process.env.SITE_PATH);
@@ -12,13 +12,20 @@ export const getStaticProps: GetStaticProps<Site> = async () => {
 export default function Articles(
   props: InferGetStaticPropsType<typeof getStaticProps>
 ) {
+  const { data }= useSite(process.env.SITE_PATH, {
+    initialData: props,
+    revalidateOnMount: true,
+  });
+
   return (
     <section className="space-y-12">
       <header className="max-w-screen-lg mx-auto mt-24">
-        <h1 className="text-4xl font-semibold tracking-wide">{props.site.name}</h1>
+        <h1 className="text-4xl font-semibold tracking-wide">
+          {data.site.name}
+        </h1>
       </header>
-      <ul className="max-w-lecture mx-auto">
-        {props.notes.map((note) => (
+      <ul className="max-w-lecture mx-auto space-y-8">
+        {data.notes.map((note) => (
           <li key={note.id}>
             <Link href="/notes/[slug]" as={`/notes/${note.path}`}>
               <a>
@@ -26,7 +33,9 @@ export default function Articles(
                   <h2 className="text-2xl font-semibold leading-none tracking-wide">
                     {note.title}
                   </h2>
-                  <p className="text-gray-600 whitespace-pre-wrap">{note.headline}</p>
+                  <p className="text-gray-600 whitespace-pre-wrap">
+                    {note.headline}
+                  </p>
                 </article>
               </a>
             </Link>
